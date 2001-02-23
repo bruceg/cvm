5e2fedb65135d3a24ef00215bec7a9a697feedc2
Bruce Guenter <bruce@untroubled.org>
2001-02-23 05:37:48 +0000

    Install cvm_unix and testcvmclient in conf_bin.

--- insthier.c
+++ insthier.c
@@ -1,1 +1,8 @@
 #include "installer.h"
+#include "conf_bin.c"
+
+void insthier(void) {
+  int bin = opendir(conf_bin);
+  c(bin, 0, "cvm_unix", -1, -1, 0755);
+  c(bin, 0, "testcvmclient", -1, -1, 0755);
+}
