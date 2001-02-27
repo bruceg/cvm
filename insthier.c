3c9bbf02a13386897cef05fcb708e46d118d12b1
Bruce Guenter <bruce@untroubled.org>
2001-02-27 23:16:15 +0000

    Fixed to account for file name changes.

--- insthier.c
+++ insthier.c
@@ -3,6 +3,6 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, 0, "cvm_unix", -1, -1, 0755);
-  c(bin, 0, "testcvmclient", -1, -1, 0755);
+  c(bin, 0, "cvm-unix", -1, -1, 0755);
+  c(bin, 0, "cvm-testclient", -1, -1, 0755);
 }
