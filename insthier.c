88d7f781f4761b9f26643fa9b08cb20b0a3ebfdb
Bruce Guenter <bruce@untroubled.org>
2001-03-22 23:59:59 +0000

    *** empty log message ***

--- insthier.c
+++ insthier.c
@@ -3,8 +3,9 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, 0, "cvm-unix",       -1, -1, 0755);
-  c(bin, 0, "cvm-unix-local", -1, -1, 0755);
-  c(bin, 0, "cvm-unix-udp",   -1, -1, 0755);
-  c(bin, 0, "cvm-testclient", -1, -1, 0755);
+  c(bin, 0, "cvm-unix",        -1, -1, 0755);
+  c(bin, 0, "cvm-unix-local",  -1, -1, 0755);
+  c(bin, 0, "cvm-unix-udp",    -1, -1, 0755);
+  c(bin, 0, "cvm-testclient",  -1, -1, 0755);
+  c(bin, 0, "cvm-benchclient", -1, -1, 0755);
 }
