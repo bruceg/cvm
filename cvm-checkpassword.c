ccad775cc7f6c76b89289ebb6a774bbf21152b0e
Bruce Guenter <bruce@untroubled.org>
2001-05-09 23:37:53 +0000

    Added a checkpassword emulator.

---
+++ cvm-checkpassword.c
@@ -0,0 +1,74 @@
+/* cvm/cvm-checkpassword.c - Checkpassword emulator for CVM
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "client.h"
+#include "hassetenv.h"
+
+static char buffer[513];
+static char* pass;
+
+void get_data(void)
+{
+  unsigned buflen;
+  unsigned rd;
+
+  for (buflen = 0; buflen < sizeof buffer; buflen += rd) {
+    do
+      rd = read(3, buffer+buflen, sizeof buffer - buflen);
+    while ((rd == (unsigned)-1) && (errno == EINTR));
+    if (rd == (unsigned)-1) exit(111);	/* Read error */
+    if (rd == 0) break;
+  }
+  if (buflen >= sizeof buffer) exit(2); /* Buffer too long */
+  if ((pass = memchr(buffer, 0, buflen)) == 0) exit(2);	/* No password */
+  ++pass;
+  if (memchr(pass, 0, buflen-(pass-buffer)) == 0) exit(2); /* No terminator */
+}
+
+int main(int argc, char** argv)
+{
+  int i;
+  const char* tokens[3];
+  
+  if (argc < 3) {
+    printf("usage: cvm-checkpassword cvmodule program [args ...]\n");
+    return 1;
+  }
+
+  get_data();
+  tokens[0] = buffer;
+  tokens[1] = pass;
+  tokens[2] = 0;
+  if ((i = authenticate(argv[1], tokens)) != 0) return i;
+
+  if (setenv("HOME", fact_directory, 1) != 0 ||
+      setenv("USER", fact_username, 1) != 0 ||
+      (fact_realname && setenv("NAME", fact_realname, 1) != 0) ||
+      setenv("SHELL", fact_shell,1) != 0 ||
+      chdir(fact_directory) != 0 ||
+      setgid(fact_groupid) != 0 ||
+      setuid(fact_userid) != 0)
+    return 111;
+
+  execvp(argv[2], argv+2);
+  return 111;
+}
