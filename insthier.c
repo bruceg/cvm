c420a2d18cc1ac0451ffd3f81658c99ae6536a1c
Bruce Guenter <bruce@untroubled.org>
2001-07-07 05:03:46 +0000

    Added new MySQL and PgSQL modules.

--- insthier.c
+++ insthier.c
@@ -3,13 +3,19 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, 0, "cvm-benchclient",   -1, -1, 0755);
-  c(bin, 0, "cvm-checkpassword", -1, -1, 0755);
+  c(bin, 0, "cvm-benchclient",     -1, -1, 0755);
+  c(bin, 0, "cvm-checkpassword",   -1, -1, 0755);
+  c(bin, 0, "cvm-mysql",           -1, -1, 0755);
+  c(bin, 0, "cvm-mysql-local",     -1, -1, 0755);
+  c(bin, 0, "cvm-mysql-udp",       -1, -1, 0755);
+  c(bin, 0, "cvm-pgsql",           -1, -1, 0755);
+  c(bin, 0, "cvm-pgsql-local",     -1, -1, 0755);
+  c(bin, 0, "cvm-pgsql-udp",       -1, -1, 0755);
   c(bin, 0, "cvm-pwfile",          -1, -1, 0755);
   c(bin, 0, "cvm-pwfile-local",    -1, -1, 0755);
   c(bin, 0, "cvm-pwfile-udp",      -1, -1, 0755);
-  c(bin, 0, "cvm-testclient",    -1, -1, 0755);
-  c(bin, 0, "cvm-unix",          -1, -1, 0755);
-  c(bin, 0, "cvm-unix-local",    -1, -1, 0755);
-  c(bin, 0, "cvm-unix-udp",      -1, -1, 0755);
+  c(bin, 0, "cvm-testclient",      -1, -1, 0755);
+  c(bin, 0, "cvm-unix",            -1, -1, 0755);
+  c(bin, 0, "cvm-unix-local",      -1, -1, 0755);
+  c(bin, 0, "cvm-unix-udp",        -1, -1, 0755);
 }
