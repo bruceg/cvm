60627545f0146bedf485d93db4d2df5db578d7b1
Bruce Guenter <bruce@untroubled.org>
2001-08-24 04:27:28 +0000

    Updated to new insthier interface.

--- insthier.c
+++ insthier.c
@@ -3,19 +3,19 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, 0, "cvm-benchclient",     -1, -1, 0755);
-  c(bin, 0, "cvm-checkpassword",   -1, -1, 0755);
-  c(bin, 0, "cvm-mysql",           -1, -1, 0755);
-  c(bin, 0, "cvm-mysql-local",     -1, -1, 0755);
-  c(bin, 0, "cvm-mysql-udp",       -1, -1, 0755);
-  c(bin, 0, "cvm-pgsql",           -1, -1, 0755);
-  c(bin, 0, "cvm-pgsql-local",     -1, -1, 0755);
-  c(bin, 0, "cvm-pgsql-udp",       -1, -1, 0755);
-  c(bin, 0, "cvm-pwfile",          -1, -1, 0755);
-  c(bin, 0, "cvm-pwfile-local",    -1, -1, 0755);
-  c(bin, 0, "cvm-pwfile-udp",      -1, -1, 0755);
-  c(bin, 0, "cvm-testclient",      -1, -1, 0755);
-  c(bin, 0, "cvm-unix",            -1, -1, 0755);
-  c(bin, 0, "cvm-unix-local",      -1, -1, 0755);
-  c(bin, 0, "cvm-unix-udp",        -1, -1, 0755);
+  c(bin, "cvm-benchclient",     -1, -1, 0755);
+  c(bin, "cvm-checkpassword",   -1, -1, 0755);
+  c(bin, "cvm-mysql",           -1, -1, 0755);
+  c(bin, "cvm-mysql-local",     -1, -1, 0755);
+  c(bin, "cvm-mysql-udp",       -1, -1, 0755);
+  c(bin, "cvm-pgsql",           -1, -1, 0755);
+  c(bin, "cvm-pgsql-local",     -1, -1, 0755);
+  c(bin, "cvm-pgsql-udp",       -1, -1, 0755);
+  c(bin, "cvm-pwfile",          -1, -1, 0755);
+  c(bin, "cvm-pwfile-local",    -1, -1, 0755);
+  c(bin, "cvm-pwfile-udp",      -1, -1, 0755);
+  c(bin, "cvm-testclient",      -1, -1, 0755);
+  c(bin, "cvm-unix",            -1, -1, 0755);
+  c(bin, "cvm-unix-local",      -1, -1, 0755);
+  c(bin, "cvm-unix-udp",        -1, -1, 0755);
 }
