34379facf8a43101429848e3ec7554bf232d383b
Bruce Guenter <bruce@untroubled.org>
2001-09-19 03:35:09 +0000

    Added credential definitions.

---
+++ credentials.h
@@ -0,0 +1,18 @@
+#ifndef CVM__CREDENTIALS__H__
+#define CVM__CREDENTIALS__H__
+
+#define CVM_CRED_ACCOUNT 1
+#define CVM_CRED_DOMAIN 2
+#define CVM_CRED_PASSWORD 3
+#define CVM_CRED_CHALLENGE 4
+#define CVM_CRED_RESPONSE 5
+
+#if 0
+extern const char* cvm_cred_account;
+extern const char* cvm_cred_domain;
+extern const char* cvm_cred_password;
+extern const char* cvm_cred_challenge;
+extern const char* cvm_cred_response;
+#endif
+
+#endif
