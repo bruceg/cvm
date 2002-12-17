0d4f9b4d5c7f96a21015e7c130f466f0a4cd200e
Bruce Guenter <bruce@untroubled.org>
2002-12-17 00:09:59 +0000

    Add a new error code: CVME_NOCRED.

--- errors.h
+++ errors.h
@@ -7,11 +7,13 @@
 #define CVME_IO 4
 #define CVME_NOFACT 5
 #define CVME_CONFIG 6
+#define CVME_NOCRED 7
 #define CVME_PERMFAIL 100
 
 /* This error code is only used by modules, to signal that the error
    is fatal and should cause module shutdown. */
-#define CVME_MASK  0x0ff
 #define CVME_FATAL 0x100
+
+#define CVME_MASK  0x0ff
 
 #endif
