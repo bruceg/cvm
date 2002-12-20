e4e3a2328f509c656c35dcab358895a1c9af238a
Bruce Guenter <bruce@untroubled.org>
2002-12-20 22:26:13 +0000

    Previous commit was missing this change.

--- cvm-pwfile.c
+++ cvm-pwfile.c
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include "pwcmp/client.h"
+#include <pwcmp/client.h>
 #include "module.h"
 
 const unsigned cvm_credential_count = 1;
