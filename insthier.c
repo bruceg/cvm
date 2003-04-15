2c92efdb5dd6ba608a67ff97e5eb2d48f8d7a528
Bruce Guenter <bruce@untroubled.org>
2003-04-15 23:23:30 +0000

    Merged SASL library into cvm package from bglibs.

--- insthier.c
+++ insthier.c
@@ -13,17 +13,20 @@ void insthier(void) {
   c(dir, "cvm-unix",            -1, -1, 0755);
 
   dir = d(home, "include",      -1, -1, 0755);
+  s(dir, "cvm-sasl.h",          "cvm/sasl.h");
   dir = d(dir,  "cvm",          -1, -1, 0755);
   c(dir, "client.h",            -1, -1, 0644);
   /* c(dir, "credentials.h",    -1, -1, 0644); Not yet included. */
   c(dir, "errors.h",            -1, -1, 0644);
   c(dir, "facts.h",             -1, -1, 0644);
   c(dir, "module.h",            -1, -1, 0644);
+  c(dir, "sasl.h",              -1, -1, 0644);
 
   dir = d(home, "lib",          -1, -1, 0755);
   cf(dir, "cvm-client.a",       -1, -1, 0644, "client.a");
   cf(dir, "cvm-command.a",      -1, -1, 0644, "command.a");
   cf(dir, "cvm-local.a",        -1, -1, 0644, "local.a");
   cf(dir, "cvm-module.a",       -1, -1, 0644, "module.a");
+  cf(dir, "cvm-sasl.a",         -1, -1, 0644, "sasl.h");
   cf(dir, "cvm-udp.a",          -1, -1, 0644, "udp.a");
 }
