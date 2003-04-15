5f9be9934f5c16c32aaecdff838d27670339b76c
Bruce Guenter <bruce@untroubled.org>
2003-04-15 21:10:43 +0000

    Include development files in the installed set.

--- insthier.c
+++ insthier.c
@@ -1,15 +1,32 @@
 #include <installer.h>
-#include "conf_bin.c"
+#include "conf_home.c"
 
 void insthier(void) {
-  int bin = opendir(conf_bin);
-  c(bin, "cvm-benchclient",     -1, -1, 0755);
-  c(bin, "cvm-checkpassword",   -1, -1, 0755);
-  c(bin, "cvm-pwfile",          -1, -1, 0755);
-  c(bin, "cvm-pwfile-local",    -1, -1, 0755);
-  c(bin, "cvm-pwfile-udp",      -1, -1, 0755);
-  c(bin, "cvm-testclient",      -1, -1, 0755);
-  c(bin, "cvm-unix",            -1, -1, 0755);
-  c(bin, "cvm-unix-local",      -1, -1, 0755);
-  c(bin, "cvm-unix-udp",        -1, -1, 0755);
+  int home = opendir(conf_home);
+  int dir;
+
+  dir = d(home, "bin",          -1, -1, 0755);
+  c(dir, "cvm-benchclient",     -1, -1, 0755);
+  c(dir, "cvm-checkpassword",   -1, -1, 0755);
+  c(dir, "cvm-pwfile",          -1, -1, 0755);
+  c(dir, "cvm-pwfile-local",    -1, -1, 0755);
+  c(dir, "cvm-pwfile-udp",      -1, -1, 0755);
+  c(dir, "cvm-testclient",      -1, -1, 0755);
+  c(dir, "cvm-unix",            -1, -1, 0755);
+  c(dir, "cvm-unix-local",      -1, -1, 0755);
+  c(dir, "cvm-unix-udp",        -1, -1, 0755);
+
+  dir = d(home, "include",      -1, -1, 0755);
+  dir = d(dir,  "cvm",          -1, -1, 0755);
+  c(dir, "client.h",            -1, -1, 0644);
+  /* c(dir, "credentials.h",    -1, -1, 0644); Not yet included. */
+  c(dir, "errors.h",            -1, -1, 0644);
+  c(dir, "facts.h",             -1, -1, 0644);
+  c(dir, "module.h",            -1, -1, 0644);
+
+  dir = d(home, "lib",          -1, -1, 0755);
+  cf(dir, "cvm-client.a",       -1, -1, 0644, "client.a");
+  cf(dir, "cvm-command.a",      -1, -1, 0644, "command.a");
+  cf(dir, "cvm-local.a",        -1, -1, 0644, "local.a");
+  cf(dir, "cvm-udp.a",          -1, -1, 0644, "udp.a");
 }
--- spec
+++ spec
@@ -8,10 +8,19 @@ Source: http://untroubled.org/cvm/cvm-@VERSION@.tar.gz
 BuildRoot: %{_tmppath}/cvm-buildroot
 URL: http://untroubled.org/cvm/
 Packager: Bruce Guenter <bruceg@em.ca>
+BuildRequires: bglibs >= 1.009
 
 %description
-This package implements the CVM interface as a client (testcvmclient)
-and as a module (cvm_unix).
+This package implements the CVM interface as a client (cvm-testclient),
+and as a module (cvm-unix, cvm-pwfile).
+
+%package devel
+Summary: Development libraries for CVM
+Group: Development/Libraries
+
+%description devel
+This package includes all the development libraries and headers for
+building CVM clients or modules.
 
 %prep
 %setup
@@ -20,15 +29,15 @@ and as a module (cvm_unix).
 echo gcc "%{optflags}" >conf-cc
 echo gcc -s >conf-ld
 echo %{_bindir} >conf-bin
-make
+make programs
 
 %install
 rm -fr %{buildroot}
-rm -f *.o conf_bin.c
-echo %{buildroot}%{_bindir} >conf-bin
+rm -f *.o conf_home.c
+echo %{buildroot}%{_prefix} >conf-home
 make installer instcheck
 
-mkdir -p %{buildroot}%{_bindir}
+mkdir -p %{buildroot}%{_prefix}
 ./installer
 ./instcheck
 
@@ -38,8 +47,9 @@ rm -rf %{buildroot}
 %files
 %defattr(-,root,root)
 %doc COPYING NEWS README *.html
-%{_bindir}/cvm-benchclient
-%{_bindir}/cvm-checkpassword
-%{_bindir}/cvm-pwfile*
-%{_bindir}/cvm-testclient
-%{_bindir}/cvm-unix*
+%{_prefix}/bin/*
+
+%files devel
+%defattr(-,root,root)
+%{_prefix}/include/*
+%{_prefix}/lib/*
