98a4a2896c35a696635ab44833356425da23e9a9
Bruce Guenter <bruce@untroubled.org>
2003-05-20 22:47:29 +0000

    Added missing "lib" prefix to installed library files.

--- insthier.c
+++ insthier.c
@@ -23,10 +23,10 @@ void insthier(void) {
   c(dir, "sasl.h",              -1, -1, 0644);
 
   dir = d(home, "lib",          -1, -1, 0755);
-  cf(dir, "cvm-client.a",       -1, -1, 0644, "client.a");
-  cf(dir, "cvm-command.a",      -1, -1, 0644, "command.a");
-  cf(dir, "cvm-local.a",        -1, -1, 0644, "local.a");
-  cf(dir, "cvm-module.a",       -1, -1, 0644, "module.a");
-  cf(dir, "cvm-sasl.a",         -1, -1, 0644, "sasl.h");
-  cf(dir, "cvm-udp.a",          -1, -1, 0644, "udp.a");
+  cf(dir, "libcvm-client.a",    -1, -1, 0644, "client.a");
+  cf(dir, "libcvm-command.a",   -1, -1, 0644, "command.a");
+  cf(dir, "libcvm-local.a",     -1, -1, 0644, "local.a");
+  cf(dir, "libcvm-module.a",    -1, -1, 0644, "module.a");
+  cf(dir, "libcvm-sasl.a",      -1, -1, 0644, "sasl.h");
+  cf(dir, "libcvm-udp.a",       -1, -1, 0644, "udp.a");
 }
