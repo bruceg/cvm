a16a4478e9ed60ce346dc0e622d7cf3c6f8515b5
Bruce Guenter <bruce@untroubled.org>
2004-11-05 18:38:19 +0000

    Updated file header comments.

--- vmlookup.c
+++ vmlookup.c
@@ -1,5 +1,5 @@
-/* lookup.c - vmailmgr CVM lookup routines
- * Copyright (C) 2003  Bruce Guenter <bruceg@em.ca>
+/* vmlookup.c - vmailmgr CVM lookup routines
+ * Copyright (C) 2004  Bruce Guenter <bruceg@em.ca>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
