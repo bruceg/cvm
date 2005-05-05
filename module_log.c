d6516b1cd71793109abb037513095e7d3cfbc95a
Bruce Guenter <bruce@untroubled.org>
2005-05-05 06:12:59 +0000

    Renamed "str" to simply "s" to avoid conflicts with the "str" data type.

--- module_log.c
+++ module_log.c
@@ -1,5 +1,5 @@
 /* cvm/module_log.c - CVM server module logging functions
- * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ * Copyright (C) 2005  Bruce Guenter <bruceg@em.ca>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ void log_request(void)
 {
   char buf[BUFSIZE+4];
   char* ptr;
-  const char* str;
+  const char* s;
   
   ptr = buf;
   switch (outbuffer[0]) {
@@ -42,9 +42,9 @@ void log_request(void)
   default: *ptr++ = '?'; break;
   }
   *ptr++ = ' ';
-  for (str = cvm_account_name; *str; ++str) *ptr++ = *str;
+  for (s = cvm_account_name; *s; ++s) *ptr++ = *s;
   *ptr++ = '@';
-  for (str = cvm_account_domain; *str; ++str) *ptr++ = *str;
+  for (s = cvm_account_domain; *s; ++s) *ptr++ = *s;
   *ptr++ = '\n';
   *ptr = 0;
   write(1, buf, ptr-buf);
