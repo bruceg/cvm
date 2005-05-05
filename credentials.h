7e503b872085d87fc284a0771d3f42291b1d6d6e
Bruce Guenter <bruce@untroubled.org>
2005-05-05 23:55:30 +0000

    Added a response type credential, to distinguish between different challenge-response mechanisms (APOP, CRAM-MD5, etc).

--- credentials.h
+++ credentials.h
@@ -5,15 +5,9 @@
 #define CVM_CRED_DOMAIN 2
 #define CVM_CRED_PASSWORD 3
 #define CVM_CRED_SECRET 4
-#define CVM_CRED_NONCE 5
+#define CVM_CRED_CHALLENGE 5
 #define CVM_CRED_RESPONSE 6
-
-#if 0
-extern const char* cvm_cred_account;
-extern const char* cvm_cred_domain;
-extern const char* cvm_cred_password;
-extern const char* cvm_cred_challenge;
-extern const char* cvm_cred_response;
-#endif
+#define CVM_CRED_RESPONSE_TYPE 7
+#define CVM_CRED_MAX 7
 
 #endif
