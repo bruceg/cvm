bfc6a4406dd320a97b7fe57fc81ae6bd026b8cf1
Bruce Guenter <bruce@untroubled.org>
2005-05-06 23:04:33 +0000

    Rewrote the version 2 protocol to be more like version 1, for efficiency and ease of implementation.

--- protocol.h
+++ protocol.h
@@ -2,12 +2,6 @@
 #define CVM__PROTOCOL__H__
 
 #define CVM1_PROTOCOL 1
-
-#define CVM2_PROTOCOL "CVM2"
-#define CVM2_REQUEST  "REQ2"
-#define CVM2_SUCCESS  "SUCC"
-#define CVM2_FAILURE  "FAIL"
-#define CVM2_BADCRED  "EBAD"
-#define CVM2_TMPFAIL  "ETMP"
+#define CVM2_PROTOCOL 2
 
 #endif
